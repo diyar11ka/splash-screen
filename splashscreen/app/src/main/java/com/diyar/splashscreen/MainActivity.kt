083a49267cbb6ba40ac26658abb670c182700542
diyar <diyar11k@outlook.com>
2024-06-20 13:03:00 +0300

    it shows a logo gif right b4 open

---
+++ splashscreen/app/src/main/java/com/diyar/splashscreen/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.diyar.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainPageActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
